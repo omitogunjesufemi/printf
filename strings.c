076219f636624f85310ee449a48f2f301485be14
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-10-19 11:06:17 +0000

    Create a function that reverses a given string

--- strings.c
+++ strings.c
@@ -17,3 +17,30 @@ unsigned int str_len(char *s)
 
 	return (i);
 }
+
+/**
+ * reverse_str - Reverses a string
+ * @str: The string to reverse
+ * Return: Nothing
+ */
+void reverse_string(char *s)
+{
+	int len, i;
+	char temp;
+
+	len = 0;
+	i = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+		i++;
+	}
+}
